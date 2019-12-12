afca3974291045f32183995b1b62911d5f804280
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2019-12-12 18:18:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if(nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    userList.appendChild(li);
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
